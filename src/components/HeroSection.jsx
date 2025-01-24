c87eb811d68faab99e81cc4ec2f87aa46947bf1e
Adeel Ahmed <ahmedadeel021@gmail.com>
2025-01-24 15:22:23 +0500

    update HeroSection.jsx and src/index.css

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,11 +3,11 @@ import herobg from '../assets/images/herosectionbg.png';
 const HeroSection = () => {
   return (
     <>
-    <div className="herosection flex justify-center items-center gap-2 flex-col text-center">
-        <img src={herobg} />
-        <h2>بسم الله الرحمن الرحيم</h2>
+    <div className="herosection flex justify-center items-center gap-7 flex-col text-center">
+        
+        <h2 className='h2_1'>بسم الله الرحمن الرحيم</h2>
         <h1>مركز نوربوتن الإسلامي</h1>
-        <h2>Norrbottens Islamiska Center</h2>
+        <h2 className='h2_2'>Norrbottens Islamiska Center</h2>
         <p>Norrbottens Islamiska Center represents a Muslim community living in <br /> 
         the northern county of Sweden, Norrbotten.</p>
     </div>
