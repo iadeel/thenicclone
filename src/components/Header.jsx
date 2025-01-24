eef1fbc3787636c740044fd597a634a2555906d0
Adeel Ahmed <ahmedadeel021@gmail.com>
2025-01-24 14:48:15 +0500

    update 5 files

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import logo from '../assets/images/logo.svg';
+import { FaFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <>
+    <div className="header flex justify-between items-center p-5" >
+        <div className="logo">
+            <img src={logo} height={99.69} />
+        </div>
+        <div className="nav">
+            <ul className='flex items-center gap-5 text-xl font-semibold text-white'>
+                <li className='text-yellow-400'>Home</li>
+                <li>About NIC  </li>
+                <li>The Masjid</li>
+                <li>Education</li>
+                <li>Team</li>
+                <li>Contact Us</li>
+                <li className='border-2 px-2 py-1 rounded-md'>Support</li>
+            </ul>
+        </div>
+        <div className="socials flex text-white gap-3">
+        <FaFacebook size={30} />
+        <FaInstagram size={30} />
+        <FaYoutube size={30}/>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import herobg from '../assets/images/herosectionbg.png';
+
+const HeroSection = () => {
+  return (
+    <>
+    <div className="herosection flex justify-center items-center gap-2 flex-col text-center">
+        <img src={herobg} />
+        <h2>بسم الله الرحمن الرحيم</h2>
+        <h1>مركز نوربوتن الإسلامي</h1>
+        <h2>Norrbottens Islamiska Center</h2>
+        <p>Norrbottens Islamiska Center represents a Muslim community living in <br /> 
+        the northern county of Sweden, Norrbotten.</p>
+    </div>
+    </>
+  )
+}
+
+export default HeroSection
